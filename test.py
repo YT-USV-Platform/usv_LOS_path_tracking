f41faf9d2e18321db83d76c5e1d663ff595e85ba
Oguzhan Kose <koseo16@itu.edu.tr>
2022-05-05 16:20:02 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,89 @@
+
+from random import randrange
+import matplotlib.pyplot as plt
+from scipy.interpolate import pchip_interpolate
+import numpy as np
+import math
+
+x_observed = np.linspace(0.0, 1.0, 11)
+y_observed = np.sin(x_observed)
+
+x = np.linspace(min(x_observed), max(x_observed), 100)
+y = pchip_interpolate(x_observed, y_observed, x)
+plt.plot(x_observed, y_observed, x, y, '-')
+plt.plot(x,y,label='pchip')
+plt.legend()
+
+
+x_current = 0.652
+y_current = 0.345
+
+error_list = np.zeros_like(x)
+min_dist = np.inf
+min_index = 0
+for i in range(len(x)):
+    error_list[i] = math.sqrt((x[i] - x_current)**2 + (y[i] - y_current)**2)
+    if error_list[i] < min_dist:
+        min_dist = error_list[i]
+        min_index = i
+
+cross_track_error = error_list[min_index]
+    
+x_closest = x[min_index]
+y_closest = y[min_index]
+
+print("Cross track error: ", cross_track_error)
+print("min_index: ", min_index)
+print("Closest point: ", x_closest, y_closest)
+
+# add a circle to current position
+plt.plot(x_current, y_current, 'o')
+
+# plot line between current position and the closest point
+plt.plot([x_current, x_closest], [y_current, y_closest], 'b--')
+
+
+
+spline_slope = (y[min_index]-y[min_index+1]) / (x[min_index]-x[min_index+1])
+los_angle = math.atan(spline_slope)
+
+print("Spline slope: ", spline_slope)
+print("LOS angle: ", los_angle)
+
+delta = 0.15
+los_point_x = x_closest + delta * math.cos(los_angle)
+los_point_y = y_closest + delta * math.sin(los_angle)
+
+# plot line of sight
+plt.plot([x_closest, los_point_x], [y_closest, los_point_y], 'r--')
+# plot line of sight point
+plt.plot(los_point_x, los_point_y, 'ro')
+
+
+psi = math.atan(math.radians(60))
+print("Psi: ", psi)
+
+sideslip_angle = 8.0 
+print("Sideslip angle: ", sideslip_angle)
+
+course_angle = psi + sideslip_angle
+print("Course angle: ", course_angle)
+
+angle_error = course_angle - los_angle
+print("Angle error: ", angle_error)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+plt.show(block=True)
+
