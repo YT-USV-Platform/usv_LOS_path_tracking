ede435a74a2a812536b38632d20ddaabb3b6d5b8
Ferhan Buyukcolak <ferhanbuyukcolak@gmail.com>
2022-06-04 20:07:56 +0300

    Update LOSTracker.py
    update all code

--- LOSTracker.py
+++ LOSTracker.py
@@ -1,162 +1,128 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from random import randrange
-import matplotlib.pyplot as plt
-from scipy.interpolate import pchip_interpolate
+
+
+
+
+
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate
+from scipy.optimize import minimize,fsolve
+from scipy.integrate  import quad       
+import scipy.misc
+import sympy as sym
 import math
 
+class LOSTracking():
 
+    def __init__(self):
+        self.Wpx=0.0
+        self.Wpy=0.0
+        self.sideslip=0.0
+        #look heading parameters
+        self.delta_min=4
+        self.delta_max=8
+        self.delta_k=2
+        self.U_max =1
+        self.U_min=0.6
+        self.y_max=80
+        self.chi_max=5
 
-class WayPoint():
+    def execute(self,eta,Wpx): 
 
-    def __init__(self, x, y):
-        self.x_pos = x
-        self.y_pos = y
-
-
-    def __str__(self):
-        return "[WayPoint] -- x: {}, y: {}".format(self.x_pos, self.y_pos)
-
-
-class LOSPathTracking:
-
-    def __init__(self, x_initial=0.4, y_initial=0.345, psi_initial=math.atan(math.radians(60)), sideslip_initial=8.0):
-
-        self.x_current = x_initial # vehicle x_location
-        self.y_current = y_initial # vehicle y_location
-
-        self.get_current_pose(vehicle_x=self.x_current, vehicle_y=self.y_current, vehicle_psi=psi_initial, vehicle_sideslip=sideslip_initial)
-        
-        print("Psi: ", self.psi)
-        print("Sideslip angle: ", self.sideslip_angle)
-        print("Course angle: ", self.course_angle)
-
-        self.delta_min = 0.15  # meters
-        self.delta_max = 0.3  # meters
-        self.delta_k = 50 # design parameter
-
-
-    # self iptal test
-    def pchip_interpolation(self, wp_list):
-
-        min_x_wp = min(wp_list, key=lambda x: x.x_pos)
-        max_x_wp = max(wp_list, key=lambda x: x.x_pos)
-
-        x = np.linspace(min_x_wp.x_pos, max_x_wp.x_pos, 100)
-        y = pchip_interpolate([wp.x_pos for wp in wp_list], [wp.y_pos for wp in wp_list], x)
-
-        return x, y
-
-
-    def get_current_pose(self, vehicle_x=None, vehicle_y=None, vehicle_psi=None, vehicle_sideslip=None):
-
-        if vehicle_x is None:
-            self.x_current = self.x_current+0.01
-        else:
-            self.x_current = vehicle_x
-
-        if vehicle_y is None:
-            self.y_current = self.y_current+0.01
-        else:
-            self.y_current = vehicle_y
-
-        if vehicle_psi is None:
-            self.psi = self.psi+0.01
-        else:
-            self.psi = vehicle_psi
-
-        if vehicle_sideslip is None:
-            self.sideslip_angle = self.sideslip_angle+0.01
-        else:
-            self.sideslip_angle = vehicle_sideslip
-
-        self.course_angle = vehicle_psi + vehicle_sideslip
+        for m in range(len(Wpx)-1):
+            x_v=eta[0] # current vehicle position x
+            y_v=eta[1] # current vehicle position y
     
+            def f(x):
+                return 0.01*x**2#40*math.sin(1/23*x)#
+
+            Wpy=[f(Wpx[0]),f(Wpx[1])]
+            print('Wpx---',Wpx)
+            print('Wpy---',Wpy)
+            print('Wpx[0]---',Wpx[0])
+            self.x_curve=np.linspace(Wpx[0],Wpx[1],100*Wpx[1])
+            self.y_curve=f(self.x_curve)
+
+            print('******-------------********')
 
 
-    def test_get_path(self):
+            ###################################################################################
+            P = (x_v, y_v)
 
-        x_list = np.linspace(0.0, 100.0, 11)
-        
-        wp_list = []
-        for i in range(11):
-            # wp_list.append(WayPoint(x_list[i], math.sin(math.radians(180*x_list[i]))))
-            wp_list.append(WayPoint(x_list[i], 2*x_list[i]))
+            def objective(X):
+                x, y = X
+                return np.sqrt((x - P[0]) ** 2 + (y - P[1]) ** 2)
 
+            def c1(X):
+                x, y = X
+                return f(x)-y
 
-        return wp_list
+            c1={'type':'eq','fun':c1}
 
+            X = minimize(objective, x0 = [0, 0],method='SLSQP',constraints= c1)
+            self.x_closest = X.x[0]  # closest point on curve -x
+            self.y_closest = X.x[1]  # closest point on curve -y 
+            ####################################################################################
+            curve_slope_angle=math.atan(scipy.misc.derivative(f,self.x_closest,dx=1e-6)) 
+            print('curve slope',curve_slope_angle)
+            #cross-track error
+            y_e=-(x_v-self.x_closest )*math.sin(curve_slope_angle)+(y_v- self.y_closest)*math.cos(curve_slope_angle) 
+            print('y_e',y_e)
+            self.var_lookhead_distance=(self.delta_max-self.delta_min)*math.exp(-self.delta_k*y_e**2)+self.delta_min
 
-    def execute(self, vehicle_x, vehicle_y, vehicle_psi, vehicle_sideslip):
-
-        self.wp_list = self.test_get_path()
-
-        self.get_current_pose(vehicle_x, vehicle_y, vehicle_psi, vehicle_sideslip)
-
-        self.x, self.y = self.pchip_interpolation(self.wp_list)
-
-        error_list = np.zeros_like(self.x)
-        min_dist = np.inf
-        min_index = 0
-        for i in range(len(self.x)):
-            error_list[i] = math.sqrt((self.x[i] - self.x_current)**2 + (self.y[i] - self.y_current)**2)
-            if error_list[i] < min_dist:
-                min_dist = error_list[i]
-                min_index = i
-
-
-        cross_track_error = error_list[min_index]
-        print("Cross track error: ", cross_track_error)
-
-
-        self.x_closest = self.x[min_index]
-        self.y_closest = self.y[min_index]
-
-
-        spline_slope = (self.y[min_index]-self.y[min_index+1]) / (self.x[min_index]-self.x[min_index+1])
-        self.los_angle = math.atan(spline_slope)
-
-
-        delta = (self.delta_max-self.delta_min)*math.exp(-1*self.delta_k*cross_track_error**2)+self.delta_min
-        print("Varying delta: ", delta)
-
-        self.los_point_x = self.x_closest + delta * math.cos(self.los_angle)
-        self.los_point_y = self.y_closest + delta * math.sin(self.los_angle)
-
-        print("Spline slope: ", spline_slope)
-        print("LOS angle: ", self.los_angle)
-
-        self.angle_error = self.course_angle - self.los_angle
-        print("Angle error: ", self.angle_error)
-        
-        return self.angle_error, cross_track_error
+            def integrand(t):
+                return math.sqrt(1+(f(t))**2)   # f matrix takip edilen path'in fonsksiyonudur.
+            
+            def function(x):
+                return quad(integrand,0,x)[0]-self.var_lookhead_distance
+            
+            x_upper_limit = fsolve(function,1.0)
+            self.x_los = x_upper_limit+self.x_closest
+            self.y_los = f(self.x_los)
 
 
 
-    def visualize_tracker(self):
 
+
+
+        # self.x_los =  self.x_closest+ self.var_lookhead_distance*math.cos(curve_slope_angle)
+        # self.y_los =  self.y_closest + self.var_lookhead_distance*math.sin(curve_slope_angle)
+
+        self.chi_r=math.atan(-y_e/abs(self.var_lookhead_distance)) # los angle 
+
+        self.chi_d=curve_slope_angle+self.chi_r # ref açı
+        error_angle=self.chi_d-eta[5] # açı hatası
+
+        # referans hız değeri
+        term1=abs(y_e)/self.y_max
+        term2=abs(error_angle)/self.chi_max
+        U_desired = max(self.U_max*(1-term1-term2)+self.U_min,self.U_min)
+        return error_angle, U_desired, y_e,self.chi_d
+
+    def los_simulation(self,eta):
         plt.clf()
 
-        plt.plot([wp.x_pos for wp in self.wp_list], [wp.y_pos for wp in self.wp_list], 'ro')
-            
-        plt.plot(self.x, self.y, label='pchip')
+        # plt.plot(self.Wpx,self.Wpy,'ro',label='Waypoints')
+        
+        plt.plot(self.x_curve,self.y_curve,'b-')
+        
+        plt.plot(eta[0],eta[1],'bo',label='vahicle position')
 
-        # print("min_index: ", min_index)
-        print("Closest point: ", self.x_closest, self.y_closest)
+        plt.plot(self.x_closest,self.y_closest,'co')
+        
+        plt.plot([eta[0],self.x_closest],[eta[1],self.y_closest],'g--',label='cross track error ')
 
-        plt.plot(self.x_current, self.y_current, 'bo')
+        plt.plot([self.x_closest,self.x_los],[self.y_closest,self.y_los],'m--',label='Varing Delta')
 
-        # plot line between current position and the closest point
-        plt.plot([self.x_current, self.x_closest], [self.y_current, self.y_closest], 'b--')
+        plt.plot(self.x_los,self.y_los,'ko')
 
-        # plot line of sight
-        plt.plot([self.x_closest, self.los_point_x], [self.y_closest, self.los_point_y], 'r--')
-        # plot line of sight point
-        plt.plot(self.los_point_x, self.los_point_y, 'go')
+        plt.plot([eta[0],eta[0]+5*math.cos(eta[5])],[eta[1],eta[1]+5*math.sin(eta[5])])
 
         plt.show(block=False)
-        plt.pause(0.05)
+
+        plt.pause(0.02)
 
 
 
