9cfc2bf4f9ec36e14af280a52792a51944de6840
Ferhan Buyukcolak <ferhanbuyukcolak@gmail.com>
2022-06-05 22:30:30 +0300

    Update LOSTracker.py
    New curvature features added

--- LOSTracker.py
+++ LOSTracker.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate
 from scipy.optimize import minimize,fsolve
+from scipy import misc
 from scipy.integrate  import quad       
-import scipy.misc
-import sympy as sym
 import math
+from splinegeneration import spline_gen
 
 class LOSTracking():
 
@@ -21,27 +16,47 @@ class LOSTracking():
         self.Wpy=0.0
         self.sideslip=0.0
         #look heading parameters
-        self.delta_min=4
-        self.delta_max=8
+        self.delta_min=3
+        self.delta_max=6
         self.delta_k=2
         self.U_max =1
         self.U_min=0.6
         self.y_max=80
         self.chi_max=5
 
-    def execute(self,eta,Wpx): 
+    def execute(self,eta,Wpx,Wpy): 
 
+
+        Spline_gen = spline_gen()
+        self.x_int,self.y_int=Spline_gen.pchip_gen(Wpx,Wpy)
+        coeff_list = Spline_gen.execute()
+        k =0
+        m = 4 # Araç etrafındaki 2 gemi boyundaki çamber
+        print('coeff_list:',coeff_list)
+    
+        dist = np.sqrt((eta[0]-Wpx[k+1])**2+(eta[1]-Wpy[k+1])**2)
+        print('dist:',dist)
+        if dist<=m:
+            coefficient = coeff_list[k][:] 
+
+            k+=1     
+            print('k',k) 
+             
+        else:
+            coefficient = coeff_list[k][:] 
+
+        print('coeff[k][1]',coefficient)
         for m in range(len(Wpx)-1):
             x_v=eta[0] # current vehicle position x
             y_v=eta[1] # current vehicle position y
     
             def f(x):
-                return 0.01*x**2#40*math.sin(1/23*x)#
-
+                # return 0.01*x**2#40*math.sin(1/23*x)#
+                return coefficient[0]*x**3+coefficient[1]*x**2+coefficient[2]*x+coefficient[3]
             Wpy=[f(Wpx[0]),f(Wpx[1])]
-            print('Wpx---',Wpx)
-            print('Wpy---',Wpy)
-            print('Wpx[0]---',Wpx[0])
+            # print('Wpx---',Wpx)
+            # print('Wpy---',Wpy)
+            # print('Wpx[0]---',Wpx[0])
             self.x_curve=np.linspace(Wpx[0],Wpx[1],100*Wpx[1])
             self.y_curve=f(self.x_curve)
 
@@ -66,14 +81,14 @@ class LOSTracking():
             self.y_closest = X.x[1]  # closest point on curve -y 
             ####################################################################################
             curve_slope_angle=math.atan(scipy.misc.derivative(f,self.x_closest,dx=1e-6)) 
-            print('curve slope',curve_slope_angle)
+            # print('curve slope',curve_slope_angle)
             #cross-track error
             y_e=-(x_v-self.x_closest )*math.sin(curve_slope_angle)+(y_v- self.y_closest)*math.cos(curve_slope_angle) 
-            print('y_e',y_e)
+            # print('y_e',y_e)
             self.var_lookhead_distance=(self.delta_max-self.delta_min)*math.exp(-self.delta_k*y_e**2)+self.delta_min
 
             def integrand(t):
-                return math.sqrt(1+(f(t))**2)   # f : path function
+                return math.sqrt(1+(f(t))**2)   # f matrix takip edilen path'in fonsksiyonudur.
             
             def function(x):
                 return quad(integrand,0,x)[0]-self.var_lookhead_distance
@@ -89,11 +104,11 @@ class LOSTracking():
 
         # self.x_los =  self.x_closest+ self.var_lookhead_distance*math.cos(curve_slope_angle)
         # self.y_los =  self.y_closest + self.var_lookhead_distance*math.sin(curve_slope_angle)
+        d = np.sqrt((self.x_closest-self.x_los)**2+(self.y_closest-self.y_los)**2)
+        self.chi_r=math.atan(-y_e/abs(d)) # los angle 
 
-        self.chi_r=math.atan(-y_e/abs(self.var_lookhead_distance)) # los angle 
-
-        self.chi_d=curve_slope_angle+self.chi_r       # ref angle
-        error_angle=self.chi_d-eta[5]                 # angle errror
+        self.chi_d=curve_slope_angle+self.chi_r # ref açı
+        error_angle=self.chi_d-eta[5] # açı hatası
 
         # referans hız değeri
         term1=abs(y_e)/self.y_max
@@ -101,12 +116,13 @@ class LOSTracking():
         U_desired = max(self.U_max*(1-term1-term2)+self.U_min,self.U_min)
         return error_angle, U_desired, y_e,self.chi_d
 
-    def los_simulation(self,eta):
+    def los_simulation(self,eta,Wpx,Wpy):
         plt.clf()
 
-        # plt.plot(self.Wpx,self.Wpy,'ro',label='Waypoints')
+        plt.plot(Wpx,Wpy,'ro',label='Waypoints')
         
-        plt.plot(self.x_curve,self.y_curve,'b-')
+        # plt.plot(self.x_curve,self.y_curve,'b-')
+        plt.plot(self.x_int,self.y_int)
         
         plt.plot(eta[0],eta[1],'bo',label='vahicle position')
 
@@ -122,13 +138,7 @@ class LOSTracking():
 
         plt.show(block=False)
 
-        plt.pause(0.02)
-
-
-
-
-
-
+        plt.pause(0.0002)
 
 
 
